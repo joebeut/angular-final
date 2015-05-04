abf23db163e69a80e0b1cbc90ce2b658b60d5e4e
Joe Beutler <jtb2133@columbia.edu>
2015-05-04 01:12:38 -0400

    completed with border-on-hover

--- public/js/directives/flash-card/flash-card.js
+++ public/js/directives/flash-card/flash-card.js
@@ -1,5 +1,8 @@
 app.directive('flashCard', function () {
     return {
+    	scope: {
+    		card: '='
+		},
         restrict: 'E',
         templateUrl: 'js/directives/flash-card/flash-card.html'
     };
